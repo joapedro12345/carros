1d47e169b881bb5a8c59130b93e69ea7d79002a9
joao <joaopedromestreff2007@gmail.com>
2024-03-25 19:55:49 -0300

    sexto commit do App

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ export default {
     },
     fontFamily: {
       'shoulders': '"Big Shoulders Display"',
-      'lexend': '"lexend Deca"'
+      'lexend': '"Lexend Deca"'
     }
   },
   plugins: [],
